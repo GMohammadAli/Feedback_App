d98628dc8814fe42349f3e7b8ed56342f9f60363
GMohammadAli <gulammohammadali@gmail.com>
2022-05-29 00:00:47 +0530

    Section 6 Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./components/pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +25,25 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route exact path="/" element={<>
+           <FeedbackForm handleAdd={addFeedback} />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+          </>
+          }>
+          </Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import Card from "../styleComponents/Card"
+import {Link} from 'react-router-dom'
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+          <h1>About Page</h1>
+          <p>This is a React app to leave feedback for a product</p>
+          <p>Version: 1.0.0</p>
+
+          <p>
+              <Link to="/">Back to Home</Link>
+          </p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
